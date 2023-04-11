7f804ab0f63ea60eda3dd960f401e21c5a576d8d
Richelle Williams <richellewilliams3@gmail.com>
2023-04-11 16:36:01 -0500

    Updated front-end page to display the movies

--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="about container">
-    <h2 class="pb-4">Movies</h2>
-    <div>
-        <div v-for="movie in movies" :key="movie.id" class="card">
-      <img :src="movie.poster" alt="Movie Poster" class="card-img-top">
-      <div class="card-body">
-        <h3 class="card-title">{{ movie.title }}</h3>
-        <p class="card-text">{{ movie.description }}</p>
+    <h2 class="pb-3">Movies</h2>
+
+    <div class="row row-cols-2 g-4">
+      <div v-for="movie in movies" :key="movie.id">
+        <div class="card mb-3">
+            <div class="col-md-4">
+              <img :src="movie.poster" alt="Movie Poster" class="card-img-top">
+            </div>
+
+            <div class="col-md-8">
+              <div class="card-body">
+                <h3 class="card-title">{{ movie.title }}</h3>
+                <p class="card-text">{{ movie.description }}</p>
+              </div>
+            </div>
+        </div>
       </div>
     </div>
-    </div>
-    
   </div>
 </template>
 
@@ -22,15 +29,22 @@ function fetchMovies(){
     fetch('/api/v1/movies')
     .then((response) => response.json())
     .then((data) => {
-        console.log(data);
-        movies.value = data;
+        //console.log(data);
+        movies.value = data.data;
     })
     .catch((error) => {
         console.log(error);
-    })
+    });
 }
 
 onMounted(()=> {
-    fetchMovies()
+    fetchMovies();
 });
 </script>
+
+<style>
+.card {
+    width: 600px;
+    flex-direction: row;
+}
+</style>
